8248684a494d7fee15b6bfc7727d0a5cce9c72e3
khoa.nguyen <khoa.nguyen@thegioiwhey.com>
2025-09-10 10:50:52 +0700

    common admin button

--- src/components/admin/ui/button/Button.tsx
+++ src/components/admin/ui/button/Button.tsx
@@ -1,5 +1,43 @@
-import React from "react";
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+import React, { ButtonHTMLAttributes, FC } from "react";
 
-export const Button = () => {
-    return <div>Button</div>;
+const buttonVariants = cva("btn", {
+    variants: {
+        variant: {
+            default: "",
+            soft: "btn-soft",
+            outline: "btn-outline",
+            dash: "btn-dash",
+        },
+        color: {
+            default: "",
+            primary: "btn-primary",
+            secondary: "btn-secondary",
+            accent: "btn-accent",
+            info: "btn-info",
+            success: "btn-success",
+            warning: "btn-warning",
+            error: "btn-error",
+        },
+    },
+    defaultVariants: {
+        variant: "default",
+        color: "default",
+    },
+});
+
+export const Button: FC<ButtonHTMLAttributes<HTMLButtonElement> & VariantProps<typeof buttonVariants>> = ({ variant, color, className = "", ...props }) => {
+    return (
+        <button
+            className={cn(
+                buttonVariants({
+                    color,
+                    variant,
+                }),
+                className
+            )}
+            {...props}
+        />
+    );
 };
---
+++ src/components/admin/ui/button/IconButton.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils/class";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import React, { ButtonHTMLAttributes, FC } from "react";
+
+export const IconButton: FC<
+  ButtonHTMLAttributes<HTMLButtonElement> & { icon: string }
+> = ({ icon, className, ...rest }) => {
+  return (
+    <button
+      className={cn(
+        "text-primary dark:hover:bg-gray-300 hover:bg-gray-100 duration-200 rounded p-1",
+        className,
+      )}
+      {...rest}
+    >
+      <Icon icon={icon} />
+    </button>
+  );
+};
