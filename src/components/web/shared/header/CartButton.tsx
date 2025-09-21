dbde60683350a09c8b69431c83323f41b440329c
hoangkhoanguyen <hoangkhoanguyen@hoangkhoanguyens-MacBook-Pro.local>
2025-09-21 20:07:28 +0700

    fix bug on safari

--- src/components/web/shared/header/CartButton.tsx
+++ src/components/web/shared/header/CartButton.tsx
@@ -6,21 +6,16 @@ import React from "react";
 
 const CartButton = () => {
   return (
-    <Link href={webRoutes.cart()}>
-      <span className="relative">
-        <Icon
-          icon={"ph:shopping-bag"}
-          className="text-web-content-1 w-11 h-11"
-        />
-        <span
-          className={cn(
-            "absolute -top-1 -right-2",
-            "w-8 aspect-square rounded-full flex justify-center items-center",
-            "bg-web-secondary-3  text-web-background-1 text-web-h4",
-          )}
-        >
-          9+
-        </span>
+    <Link href={webRoutes.cart()} className="relative">
+      <Icon icon={"ph:shopping-bag"} className="text-web-content-1 w-11 h-11" />
+      <span
+        className={cn(
+          "absolute -top-1 -right-2",
+          "w-8 aspect-square rounded-full flex justify-center items-center",
+          "bg-web-secondary-3  text-web-background-1 text-web-h4",
+        )}
+      >
+        9+
       </span>
     </Link>
   );
