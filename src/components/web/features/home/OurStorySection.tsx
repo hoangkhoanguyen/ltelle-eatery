915b01f820c4bd4b996481975b682a7c2182e123
hoangkhoanguyen <hoangkhoanguyen@hoangkhoanguyens-MacBook-Pro.local>
2025-09-21 17:09:32 +0700

    quality image

--- src/components/web/features/home/OurStorySection.tsx
+++ src/components/web/features/home/OurStorySection.tsx
@@ -14,6 +14,7 @@ export const OurStorySection = () => {
                   alt="Our Story"
                   fill
                   className="object-cover"
+                  quality={100}
                 />
               </div>
             </div>
