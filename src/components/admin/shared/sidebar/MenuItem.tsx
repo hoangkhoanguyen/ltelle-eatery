386c9a65ab201341ee62732382d39aeefb4c89bd
hoangkhoanguyen <anhhaido28@gmail.com>
2025-10-04 19:04:44 +0700

    update animation sidebar

--- src/components/admin/shared/sidebar/MenuItem.tsx
+++ src/components/admin/shared/sidebar/MenuItem.tsx
@@ -1,11 +1,5 @@
-"use client";
 import Icon from "@/components/common/Icon";
 import { cn } from "@/lib/utils";
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-} from "@headlessui/react";
 import Link from "next/link";
 import React, { FC } from "react";
 
@@ -50,37 +44,26 @@ export const MenuItem: FC<IMenuItem & { pathname: string }> = ({
   }
 
   return (
-    <Disclosure as="div" className="w-full max-w-md">
-      <DisclosureButton className="flex justify-between items-center rounded-xl duration-200 gap-3 py-2.5 px-4 hover:bg-slate-600 w-full cursor-pointer">
-        <div className="flex-1 flex items-center gap-3">
-          {icon && (
-            <Icon
-              className={cn("text-xl text-white")}
-              icon={isActive ? activeIcon! : icon}
-            />
-          )}
-          <span
-            className={cn(
-              "text-sm font-medium duration-200 truncate text-white",
-            )}
-          >
-            {label}
-          </span>
-        </div>
-        <Icon icon="ph:caret-right" className="text-lg text-white" />
-      </DisclosureButton>
-      <div className="overflow-hidden py-2">
-        <DisclosurePanel
-          transition
-          className="origin-top transition duration-200 ease-out data-closed:h-0 data-open:h-auto data-closed:opacity-0"
+    <details>
+      <summary className="flex-1 flex items-center gap-3 w-full py-2.5 px-4 hover:bg-slate-600 cursor-pointer duration-200 rounded-xl">
+        {icon && (
+          <Icon
+            className={cn("text-xl text-white")}
+            icon={isActive ? activeIcon! : icon}
+          />
+        )}
+        <span
+          className={cn("text-sm font-medium duration-200 truncate text-white")}
         >
-          <div className="ps-8">
-            {children.map((child, index) => (
-              <MenuItem key={index} {...child} pathname={pathname} />
-            ))}
-          </div>
-        </DisclosurePanel>
+          {label}
+        </span>
+      </summary>
+
+      <div className="ps-8 py-2 flex flex-col items-stretch gap-2">
+        {children.map((child, index) => (
+          <MenuItem key={index} {...child} pathname={pathname} />
+        ))}
       </div>
-    </Disclosure>
+    </details>
   );
 };
