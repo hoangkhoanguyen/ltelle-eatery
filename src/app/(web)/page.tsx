aecd66d462eec3a0dafde8c4256eee9d232bf647
hoangkhoanguyen <anhhaido28@gmail.com>
2025-10-12 20:49:03 +0700

    test home

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -7,7 +7,7 @@ import { WhyChooseUsSection } from "@/components/web/shared/WhyChooseUsSection";
 import { getUIConfigsByKeyCached } from "@/services/configs";
 import React from "react";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 const HomePage = async () => {
   const homeConfig = await getUIConfigsByKeyCached("homepage");
