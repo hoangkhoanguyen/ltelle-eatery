6b563c3557ce3bf0c36526e0a618cb9e7d6fae18
hoangkhoanguyen <anhhaido28@gmail.com>
2025-11-21 09:30:42 +0700

    change admin logo and toggle mobile sidebar

--- src/components/admin/shared/header/Header.tsx
+++ src/components/admin/shared/header/Header.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from "react";
+import { ToggleSidebar } from "../sidebar/ToggleSidebar";
 
 export default function Header({
   title,
@@ -12,10 +13,15 @@ export default function Header({
   return (
     <>
       <div className="fixed z-30 duration-200 start-0 lg:start-64 end-0 top-0 bg-white px-5">
-        <div className="flex justify-between items-center gap-5 h-12">
-          <p className="text-gray-900 font-semibold">{title}</p>
-          {center}
-          <div>{actions}</div>
+        <div className="flex items-center gap-5">
+          <div className="lg:hidden">
+            <ToggleSidebar />
+          </div>
+          <div className="flex-1 flex justify-between items-center gap-5 h-12">
+            <p className="text-gray-900 font-semibold">{title}</p>
+            {center}
+            <div>{actions}</div>
+          </div>
         </div>
       </div>
       <div className="h-12"></div>
--- src/components/admin/shared/sidebar/Sidebar.tsx
+++ src/components/admin/shared/sidebar/Sidebar.tsx
@@ -5,23 +5,23 @@ import { Menu } from "./Menu";
 import { cn } from "@/lib/utils";
 import { Icon } from "@iconify/react";
 import { useLogout } from "@/hooks/admin/features/auth/useLogout";
+import { useAdminConfigs } from "@/store";
+import { Dialog, DialogPanel } from "@headlessui/react";
 
 const Sidebar = () => {
+  const isOpenSidebar = useAdminConfigs((state) => state.isSidebarOpen);
+  const onClose = useAdminConfigs((state) => state.closeSidebar);
   const { mutate: logout, isPending } = useLogout();
 
   const handleLogout = () => {
     logout();
   };
 
-  return (
-    <div
-      className={
-        "fixed top-0 bottom-0 start-0 bg-slate-900 dark:bg-gray-900 duration-200 w-64 -translate-x-full rtl:translate-x-full lg:translate-x-0 flex flex-col"
-      }
-    >
+  const renderSidebar = () => (
+    <div className="bg-slate-900 dark:bg-gray-900 flex flex-col h-full w-64">
       <div className={cn("flex items-center gap-4 pt-4 duration-200 px-5")}>
         <Image
-          src={"/assets/static/logo.svg"}
+          src={"/assets/static/light-logo.png"}
           width={32}
           height={32}
           alt="L'telle Eatery"
@@ -66,6 +66,35 @@ const Sidebar = () => {
         </button>
       </div>
     </div>
+  );
+
+  return (
+    <>
+      <div
+        className={
+          "fixed z-40 top-0 bottom-0 start-0 duration-200 -translate-x-full rtl:translate-x-full lg:translate-x-0"
+        }
+      >
+        {renderSidebar()}
+      </div>
+
+      <Dialog
+        open={isOpenSidebar}
+        onClose={onClose}
+        transition
+        className="fixed inset-0 z-40 flex w-screen items-center justify-start bg-black/30 transition duration-200 ease-out data-closed:opacity-0 lg:hidden"
+      >
+        <DialogPanel
+          transition
+          data-theme="light"
+          className={cn(
+            "h-screen bg-white duration-200 transition data-closed:-translate-x-full rtl:data-closed:translate-x-full ",
+          )}
+        >
+          {renderSidebar()}
+        </DialogPanel>
+      </Dialog>
+    </>
   );
 };
 export default Sidebar;
